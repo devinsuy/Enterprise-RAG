a954b63f53ed371d2d91cbabc511e3c656e2e41f
devinsuy <devinsuy@berkeley.edu>
2024-07-21 17:52:09 -0700

    Point client host config to remote api

--- application/web-app/src/config.ts
+++ application/web-app/src/config.ts
@@ -1,4 +1,5 @@
-export const HOST = 'http://localhost:8000'
+// export const HOST = 'http://localhost:8000'
+export const HOST = 'http://44.208.85.132'
 export const API_ENDPOINTS = {
   chat: `${HOST}/api/v1/chat`
 }
