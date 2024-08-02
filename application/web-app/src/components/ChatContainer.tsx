d7aafe0d01138b8781805a29fafdff4d72b76919
devinsuy <devinsuy@berkeley.edu>
2024-08-01 23:56:46 -0700

    Add dynamic dimensions for chat window

--- application/web-app/src/components/ChatContainer.tsx
+++ application/web-app/src/components/ChatContainer.tsx
@@ -5,7 +5,23 @@ import { MessageList } from './MessageList'
 // import { TunersList } from './TunersList'
 
 export const ChatContainer: React.FC = () => (
-  <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', backgroundColor: '#fff', marginLeft: 10, marginRight: 10, marginTop: 3, padding: 2, borderRadius: 2, boxShadow: 1, height: '100%', maxHeight: '65vh' }}>
+  <Box sx={{
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 3,
+    padding: 2,
+    borderRadius: 2,
+    boxShadow: 1,
+    minHeight: '65%',
+    maxHeight: '65%',
+    width: '100%',
+    maxWidth: '45%',
+    justifyContent: 'center'
+  }}>
     <MessageList />
     {/* <MessageInput /> */}
   </Box>
